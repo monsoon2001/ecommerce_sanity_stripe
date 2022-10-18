4f065dddfbd70792897a89b9773f760c6b361bbf
Monsoon Parajuli <parajuli.monsoon2001@gmail.com>
2022-10-18 08:57:52 +0545

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+// next.config.js
+module.exports = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
 }
-
-module.exports = nextConfig
